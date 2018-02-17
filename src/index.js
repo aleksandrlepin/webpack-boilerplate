7a7a40039529dced4587607f780e3bd2affc8d19
aleksandrlepin <yanektulho@gmail.com>
2018-02-17 23:20:26 +0200

    Revert "finish lepin_a-js_17"
    This reverts commit 1f041c0a4fb5a60da2296f58ab9598364c1b5fa8.

--- src/index.js
+++ src/index.js
@@ -1,33 +1,10 @@
-import { Timer } from './js/timer';
-import { Shape, Rectangle, Circle } from './js/shape';
+import {add} from './js/add';
 import './css/main.scss';
+import cat from './img/interface.svg';
 
-$(document).ready(function () {
-  // the "href" attribute of the modal trigger must specify the modal ID that wants to be triggered
-  $('.modal').modal();
-});
-
-const rectangle = new Rectangle;
-const circle = new Circle;
-
-rectangle.draw();
-circle.draw();
-
-const t1 = new Timer(1, 2, 3);
-const t2 = new Timer(4, 5, 6);
-const t3 = new Timer(7, 8, 9);
-
-console.log('t1: ', t1);
-console.log('t2: ', t2);
-console.log('t3: ', t3);
-
-const stopwatch = new Timer(0, 0, 0);
-const startBinded = stopwatch.start.bind(stopwatch);
-const stopBinded = stopwatch.stop.bind(stopwatch);
-const getTimeBinded = stopwatch.getTime.bind(stopwatch);
-const timeToNYBinded = Timer.timeToNY.bind(Timer);
-
-document.querySelector('#startBtn').addEventListener('click', startBinded);
-document.querySelector('#stopBtn').addEventListener('click', stopBinded);
-document.querySelector('#getTimeBtn').addEventListener('click', getTimeBinded);
-document.querySelector('#toNY').addEventListener('click', timeToNYBinded);
+let image = document.querySelector('#cat');
+image.setAttribute('src', cat);
+//
+// console.log('Hello');
+//
+// console.log(add(5,6));
--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
         })
       },
       {
-        test: /\.(png|jpg|gif|svg)$/,
+        test: /\.(png|jpg|gif)$/,
         include: SRC__DIR,
         use: [
           {
